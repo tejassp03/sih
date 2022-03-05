c5e1bb6df766d8bb5bb52fd432449cb71dcb08a6
tejassp03 <tejassp03@gmail.com>
2022-03-05 15:15:43 +0530

    Navbar done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { Container, Navbar,Nav} from 'react-bootstrap';
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar sticky="top" variant="dark" style={{backgroundColor:'royalblue',padding:15}}>
+    <Container style={{marginLeft:15}}>
+      <Navbar.Brand href="#home" >
+     <b>College Search</b> 
+   &emsp;&emsp;
+      </Navbar.Brand>
+     <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+
+
+
     </div>
   );
 }
+
 
 export default App;
